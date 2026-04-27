0f968709184df93674d13528c049a17b53d119ff
Student <foobar@sr.htlweiz.at>
2026-04-27 11:26:28 +0200

    Spielfeld_noch_nicht ganz_fertig

---
+++ Library/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Library;
+
+public class Board{
+
+    public string line1 = "+---+---+---+---+---+---+---+---+";
+    public string line2 = "|   ";
+    public string line3 = "| # ";
+
+    public override string ToString()
+    {
+        string result = "";
+        for(int y = 0; y < 8; y++){
+            for(int x = 0; x < 8; x++){
+                if(y / 2 % == 0){
+                    result += line1;
+                    result += "\n";
+                    
+                }
+                
+            }
+        }
+        return result;
+    }
+    
+}
+
+
---
+++ Program/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Game;
+
+using Library;
+
+    public class Game(){
+        public static int Main(){
+            Board test = new();
+            Console.WriteLine(test.ToString());
+            return 0;
+        }
+    }
+    
